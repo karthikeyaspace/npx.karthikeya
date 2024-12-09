a3b793b8b0d91202c18b5695f845a123cbae5419
Karthikeya <veruturikarthikeya@gmail.com>
2024-12-09 17:51:04 +0530

    refactor: github api at api.itskv.me

--- index.ts
+++ index.ts
@@ -2,25 +2,42 @@
 
 import terminalLink from "terminal-link";
 import chalk from "chalk";
-import { Octokit } from "@octokit/rest";
 
-const username = "karthikeyaspace";
-const octokit = new Octokit();
+const BASE_URL = "https://api.itskv.me";
 
-const n = 7;
-
-const fetchTotalCommits = async () => {
+const fetchData = async () => {
   try {
-    const res = await octokit.search
-      .commits({
-        q: `author:${username}`,
-      })
-      .then((res) => res.data.total_count);
-
-    console.log(chalk.greenBright(`${chalk.red(res)} Total contributions`));
+    const res = await fetch(`${BASE_URL}/npx/me`).then((res) => res.json());
+    if (res.success) {
+      return res.data;
+    }
+    throw new Error("Failed to fetch data");
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
+};
+
+const printRepo = (repo: {
+  name: string;
+  description: string;
+  url: string;
+}) => {
+  console.log(
+    chalk.blueBright(
+      `-> ${chalk.redBright(repo.name)} - ${terminalLink("", repo.url)}`
+    )
+  );
+  console.log(
+    chalk.yellowBright(
+      `   ${repo.description ? repo.description : "Still working on it"}\n`
+    )
+  );
+};
+
+const printSocial = (social: { name: string; url: string }) => {
+  console.log(
+    chalk.greenBright(`${social.name} - ${terminalLink("", social.url)}`)
+  );
 };
 
 const printme = async (): Promise<void> => {
@@ -28,91 +45,47 @@ const printme = async (): Promise<void> => {
     console.log("\n");
     console.log(
       chalk.blue(`
-            
-██╗  ██╗ █████╗ ██████╗ ████████╗██╗  ██╗██╗██╗  ██╗███████╗██╗   ██╗ █████╗ 
-██║ ██╔╝██╔══██╗██╔══██╗╚══██╔══╝██║  ██║██║██║ ██╔╝██╔════╝╚██╗ ██╔╝██╔══██╗
-█████╔╝ ███████║██████╔╝   ██║   ███████║██║█████╔╝ █████╗   ╚████╔╝ ███████║
-██╔═██╗ ██╔══██║██╔══██╗   ██║   ██╔══██║██║██╔═██╗ ██╔══╝    ╚██╔╝  ██╔══██║
-██║  ██╗██║  ██║██║  ██║   ██║   ██║  ██║██║██║  ██╗███████╗   ██║   ██║  ██║
-╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝                                                     
-
+              ██╗  ██╗ █████╗ ██████╗ ████████╗██╗  ██╗██╗██╗  ██╗███████╗██╗   ██╗ █████╗ 
+              ██║ ██╔╝██╔══██╗██╔══██╗╚══██╔══╝██║  ██║██║██║ ██╔╝██╔════╝╚██╗ ██╔╝██╔══██╗
+              █████╔╝ ███████║██████╔╝   ██║   ███████║██║█████╔╝ █████╗   ╚████╔╝ ███████║
+              ██╔═██╗ ██╔══██║██╔══██╗   ██║   ██╔══██║██║██╔═██╗ ██╔══╝    ╚██╔╝  ██╔══██║
+              ██║  ██╗██║  ██║██║  ██║   ██║   ██║  ██║██║██║  ██╗███████╗   ██║   ██║  ██║
+              ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝                                                     
         `)
     );
 
-    const { data: user } = await octokit.users.getByUsername({
-      username: username,
-    });
+    console.log("\nMy preferred stack: ");
+    console.log(chalk.greenBright("TypeScript"));
+    console.log(chalk.greenBright("GO\n"));
 
-    console.log(chalk.bold("\n📊 gitHub stats:"));
-    console.log(chalk.greenBright("@" + user.login));
-    console.log(
-      chalk.red(user.public_repos) + chalk.greenBright(" repositories")
-    );
-    await fetchTotalCommits();
+    const github = await fetchData();
+
+    const { profile, recentRepos: repos, commits } = github;
+
+    console.log(terminalLink("@" + profile.username, profile.url));
+    console.log(chalk.yellow(profile.name));
+    console.log(chalk.cyan(profile.bio));
+    console.log(chalk.red(profile.repos) + chalk.greenBright(" repositories"));
+    console.log(chalk.red(commits) + chalk.greenBright(" contributions"));
 
     console.log(chalk.bold("\n📜 my recent projects:"));
 
-    const printTopNRepos = async (n: number) => {
-      const { data: repos } = await octokit.repos.listForUser({
-        username: username,
-        sort: "updated",
-        per_page: n,
-      });
-      repos.forEach((repo, index) => {
-        console.log(
-          chalk.blueBright(
-            `-> ${chalk.redBright(repo.name)} - ${terminalLink(
-              "",
-              repo.html_url
-            )}`
-          )
-        );
-        console.log(
-          chalk.yellowBright(
-            `   ${
-              repo.description ? repo.description : "Still working on it"
-            }\n`
-          )
-        );
-      });
-    };
+    repos.forEach(
+      (repo: { name: string; description: string; url: string }) => {
+        printRepo(repo);
+      }
+    );
 
-    await printTopNRepos(Number(n));
+    console.log("\n");
 
-    console.log("\n\n");
-    console.log(
-      chalk.greenBright(
-        terminalLink(
-          "Visit my Github profile",
-          chalk.blueBright("https://github.com/karthikeyaspace")
-        )
-      )
-    );
-    console.log(
-      chalk.greenBright(
-        terminalLink(
-          "View my portfolio",
-          chalk.blueBright("https://kv3.vercel.app")
-        )
-      )
-    );
-    console.log(
-      chalk.greenBright(
-        terminalLink(
-          "Connect with me on Linkedin ",
-          chalk.blueBright("https://linkedin.com/in/karthikeyaveruturi")
-        )
-      )
-    );
-    console.log(
-      chalk.greenBright(
-        terminalLink(
-          "Follow me on X ",
-          chalk.blueBright("https://x.com/karthikeyaspace")
-        )
-      )
-    );
-    console.log("\n\n");
+    printSocial({ name: "Github", url: "https://github.com/karthikeyaspace" });
+    printSocial({ name: "X", url: "https://x.com/karthikeyaspace" });
+    printSocial({
+      name: "LinkedIn",
+      url: "https://linkedin.com/in/karthikeyaveruturi",
+    });
+
+    console.log("\n");
   } catch (error) {
     console.error(error);
   }
